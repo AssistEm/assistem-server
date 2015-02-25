bdbaafeb1a78589dd8ee17917ae962925e8829c9
s4lly <jaime.accounts@gmail.com>
2015-02-25 15:25:50 -0600

    forgot to remove dummy code

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -2,11 +2,7 @@ var passport = require('passport');
 var jwt = require('jsonwebtoken');
 var config = require('../config/environment');
 
-exports.isAuthenticated = passport.authenticate('bearer',
-	{session: false}, function(err, user, info) {
-																								console.log("jlaskfjlsadkjflsajfldj");
-																								console.log(res);
-	});
+exports.isAuthenticated = passport.authenticate('bearer', {session: false});
 
 exports.createToken = function(payload) {
 	return jwt.sign(payload, config.secrets.token/*, {expiresInMinutes: 60*5}*/);
