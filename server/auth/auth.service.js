a5f214c6172284f0290f34aadb6baef66ef8ce02
s4lly <jaime.accounts@gmail.com>
2015-02-25 14:12:23 -0600

    removed expiration time for json token, TODO review custom callback to passport bearer strategy and return 401 status code upon json expiration

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -2,8 +2,12 @@ var passport = require('passport');
 var jwt = require('jsonwebtoken');
 var config = require('../config/environment');
 
-exports.isAuthenticated = passport.authenticate('bearer', {session: false});
+exports.isAuthenticated = passport.authenticate('bearer',
+	{session: false}, function(err, user, info) {
+																								console.log("jlaskfjlsadkjflsajfldj");
+																								console.log(res);
+	});
 
 exports.createToken = function(payload) {
-	return jwt.sign(payload, config.secrets.token, {expiresInMinutes: 60*5});
+	return jwt.sign(payload, config.secrets.token/*, {expiresInMinutes: 60*5}*/);
 };
