9f85c0631f301802ad09ddcd6195041c90a27793
s4lly <jaime.accounts@gmail.com>
2015-03-02 18:49:05 -0600

    created basic structure for groceries endpoints

---
+++ server/api/community/grocery/grocery.controller.js
@@ -0,0 +1,25 @@
+var Grocery = require('./grocery.model');
+
+exports.index = function(req, res) {
+	res.send({msg: 'foo'});
+};
+
+exports.createItem = function(req, res) {
+	res.send({msg: 'foo'});
+};
+
+exports.updateItem = function(req, res) {
+	res.send({msg: 'foo'});
+};
+
+exports.volunteerItem = function(req, res) {
+	res.send({msg: 'foo'});
+};
+
+exports.deleteItem = function(req, res) {
+	res.send({msg: 'foo'});
+};
+
+exports.autocompleteItem = function(req, res) {
+	res.send({msg: 'foo'});
+};
