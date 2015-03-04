aede208cbe33e13f2ee1244f2170ccbc7556966d
Grindlemire <grindlemire@yahoo.com>
2015-03-03 18:00:30 -0600

    added grocery delete function and corrected the index.js for the grocery items endpoint

--- server/api/community/grocery/grocery.controller.js
+++ server/api/community/grocery/grocery.controller.js
@@ -1,4 +1,5 @@
 var Grocery = require('./grocery.model');
+var _ = require('lodash');
 
 /*
  * Function that takes the title(optional) or location(optional)
@@ -30,6 +31,10 @@ exports.index = function(req, res) {
 	});
 };
 
+/*
+ * Function that takes the optional grocery item properties,
+ * creates a grocery item, and returns the list with the new item.
+ */
 exports.addItem = function(req, res) {
 	// JSON: {title:String, description:String, location:String, quantity:String, urgency:Date(PASS AS STRING)}
 	var b = req.body;
@@ -64,8 +69,43 @@ exports.volunteerItem = function(req, res) {
 	res.send({msg: 'foo'});
 };
 
+/*
+ * Function that takes no arguments (the id of the item is in the url),
+ * deletes the item from the list and returns 204 (nothing).
+ */
 exports.deleteItem = function(req, res) {
-	res.send({msg: 'foo'});
+	grocery_item_id = req.params.item_id;
+	var grocery_id = req.community.grocery_list_id;
+	var item_list = undefined;
+
+	Grocery.findOne({'_id' : grocery_id}, function(err, grocery){
+		if(err){
+			next(err);
+		}
+		else{
+			var removed = false;
+			item_list = grocery.item_list;
+			for (var i = item_list.length - 1; i >= 0; i--){
+				if(item_list[i]._id == grocery_item_id){
+					item_list.splice(i,1);
+					removed = true;
+				}
+			}
+			if(!removed){
+				res.send(404);
+			}
+			else{
+				grocery.save(function(err){
+					if(err){
+						next(err);
+					}
+					else{
+						res.send(204);
+					}
+				});
+			}
+		}
+	});
 };
 
 exports.autocompleteItem = function(req, res) {
