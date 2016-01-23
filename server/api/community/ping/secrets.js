f3b0c218b459a0b6ba53f4d86d26266d1dedbd7d
Hubert Theodore <hubert@yup.com>
2016-01-23 15:26:36 -0600

    secrets added

---
+++ server/api/community/ping/secrets.js
@@ -0,0 +1,7 @@
+var secrets = {
+        SNS_KEY_ID : "AKIAIBARKMHMWSFNRFLA",
+        SNS_ACCESS_KEY : "Q3x5z9ylpHLVFbC6+TKqJSe7URiAX4APgOpra7cO",
+        SNS_ANDROID_ARN : "bla",
+};
+
+module.exports = secrets;
